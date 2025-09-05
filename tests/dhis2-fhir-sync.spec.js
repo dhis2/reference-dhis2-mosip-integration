80b9cb454a674e8fa0539738a9c4aa9ba692b996
cjmamo <823038+cjmamo@users.noreply.github.com>
2025-09-05 16:05:35 +0200

    fix: solve errors following rebase

--- tests/dhis2-fhir-sync.spec.js
+++ tests/dhis2-fhir-sync.spec.js
@@ -7,16 +7,9 @@ test.beforeEach(async function({
     request
 }) {
     expect((await request.delete(
-        "http://localhost:8081/fhir/Consent", {
-            "params": {
-                "patient.identifier": "urn:dhis2:anc:uniqueId|8879798"
-            }
-        })).status()).toBe(200);
-
-    expect((await request.delete(
         "http://localhost:8081/fhir/Patient", {
             "params": {
-                "identifier": "urn:dhis2:anc:uniqueId|8879798"
+                "identifier": "http://fhir.health.gov.lk/ips/identifier/phn|12345678"
             }
         })).status()).toBe(200);
 });
@@ -35,65 +28,83 @@ test("should successfully sync tracked entity with EHR",
 
         const trackedEntity = {
             "trackedEntities": [{
+                "attributes": [{
+                    "attribute": "VQl0wK3eqiw",
+                    "value": "Jane Doe"
+                }, {
+                    "attribute": "CSZevH4P5yV",
+                    "value": "ANC00000002"
+                }, {
+                    "attribute": "M6NNPC3hNrb",
+                    "value": "200012345679"
+                }, {
+                    "attribute": "p7zizFkC6Lv",
+                    "value": "Female"
+                }, {
+                    "attribute": "IrUmPkFMDU5",
+                    "value": "12345678"
+                }, {
+                    "attribute": "u5AESfSOhIG",
+                    "value": "28"
+                }, {
+                    "attribute": "Yie7mOY913J",
+                    "value": "1997-08-01"
+                }, {
+                    "attribute": "gGAQeOr1Pgu",
+                    "value": "+94712345678"
+                }, {
+                    "attribute": "EOMGwaUTMrU",
+                    "value": "123 Main Street, 1234 Akurana, Kandy, Central Province, LK"
+                }],
                 "enrollments": [{
                     "attributes": [{
-                        "attribute": "PpEGiQurAll",
-                        "value": "8879798"
+                        "attribute": "p7zizFkC6Lv",
+                        "value": "Female"
                     }, {
-                        "attribute": "sB1IHYu2xQT",
-                        "value": "John"
+                        "attribute": "CSZevH4P5yV",
+                        "value": "ANC00000002"
                     }, {
-                        "attribute": "ENRjVGxVL6l",
-                        "value": "Doe"
+                        "attribute": "M6NNPC3hNrb",
+                        "value": "200012345679"
                     }, {
-                        "attribute": "NI0QRzJvQ0k",
-                        "value": "1990-04-05"
+                        "attribute": "IrUmPkFMDU5",
+                        "value": "12345678"
                     }, {
-                        "attribute": "QjdN4mhh4UN",
-                        "value": "true"
+                        "attribute": "u5AESfSOhIG",
+                        "value": "28"
                     }, {
-                        "attribute": "rYnea37ReDs",
-                        "value": "true"
+                        "attribute": "Yie7mOY913J",
+                        "value": "1997-08-01"
                     }, {
-                        "attribute": "MRGgEyilusR",
-                        "value": "true"
+                        "attribute": "gGAQeOr1Pgu",
+                        "value": "+94712345678"
                     }, {
-                        "attribute": "ruUzdQRiYpy",
-                        "value": "true"
+                        "attribute": "VQl0wK3eqiw",
+                        "value": "Joe Doe"
                     }, {
-                        "attribute": "AoOp84H5Vt1",
-                        "value": "true"
-                    }, {
-                        "attribute": "NihUionWia1",
-                        "value": "true"
-                    }, {
-                        "attribute": "qJdyXIggXXJ",
-                        "value": "true"
-                    }, {
-                        "attribute": "B6TnnFMgmCk",
-                        "value": "35"
+                        "attribute": "EOMGwaUTMrU",
+                        "value": "123 Main Street, 1234 Akurana, Kandy, Central Province, LK"
                     }],
-                    "enrolledAt": "2025-09-04",
+                    "enrolledAt": "2025-09-05",
                     "events": [{
-                        "orgUnit": "oQlmngiVAes",
-                        "program": "WSGAb5XwJ3Y",
-                        "programStage": "XznDErihed9",
-                        "scheduledAt": "2025-09-04",
+                        "orgUnit": "Tnl7qgZh7zL",
+                        "program": "eozjj9UivfS",
+                        "programStage": "LWJcStrI6kM",
+                        "scheduledAt": "2025-09-05",
                         "status": "SCHEDULE"
                     }, {
-                        "orgUnit": "oQlmngiVAes",
-                        "program": "WSGAb5XwJ3Y",
-                        "programStage": "eR4sNwxkd9Q",
-                        "scheduledAt": "2025-09-04",
-                        "status": "SCHEDULE"
+                        "orgUnit": "Tnl7qgZh7zL",
+                        "program": "eozjj9UivfS",
+                        "programStage": "GX0z9IXFaso",
+                        "scheduledAt": "2025-09-05",
+                        "status": "SCHEDULE",
                     }],
-                    "occurredAt": "2025-09-04",
-                    "orgUnit": "oQlmngiVAes",
-                    "program": "WSGAb5XwJ3Y",
-                    "status": "ACTIVE"
+                    "occurredAt": "2025-09-05",
+                    "orgUnit": "Tnl7qgZh7zL",
+                    "program": "eozjj9UivfS"
                 }],
-                "orgUnit": "oQlmngiVAes",
-                "trackedEntityType": "MCPQUTHX1Ze"
+                "orgUnit": "Tnl7qgZh7zL",
+                "trackedEntityType": "MxdEsVAegt5"
             }]
         }
 
